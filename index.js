5f4e4053b6e6c17af26a1f35fb6386663f225e90
Mike Duran-Mitchell <mike.mitchell.nyc@gmail.com>
2018-01-04 12:05:15 -0800

    added an object to include status of what was done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const argv = require("yargs")
             describe: "User you want to target with your program",
             demandOption: true,
             type: "array",
-            default: ["F0REM4N"]
+            default: ["F0REM4N", "spez", "gallowboob"]
         },
         limit: {
             alias: "l",
@@ -33,35 +33,68 @@ const credentials = require("./client_credentials.json");
 
 const r = new snoowrap(credentials);
 
+r.config({ retryErrorCodes: [503] });
+
 const targets = argv.targets;
 
-const messages = new Set([]);
+let messages = [];
 
-targets.forEach(target => {
-    messages.add(`downvoting ${target}`);
+let count = 0;
+
+const errorFiveOhThree =
+    'StatusCodeError: 503 - "<!doctype html><html><title>Ow! -- reddit.com</title><style>body{text-align:center;position:absolute;top:50%;margin:0;margin-top:-275px;width:100%}h2,h3{color:#555;font:bold 200%/100px sans-serif;margin:0}h3,p{color:#777;font:normal 150% sans-serif}p{font-size: 100%;font-style:italic;margin-top:2em;}</style><img src=//www.redditstatic.com/trouble-afoot.jpg alt=""><h2>all of our servers are busy right now</h2><h3>please try again in a minute</h3><p>(error code: 503)';
+
+targets.forEach((target, index, array) => {
+    console.log(`Starting to downvote ${target}`);
+    messages.push({
+        target: target,
+        successfullyDownvoted: 0,
+        olderThanThirty: 0,
+        fiveOhThreeErrors: 0
+    });
     r
         .getUser(target)
         .getComments({ limit: argv.limit })
         .then(comments =>
-            comments.map(comment => {
-                r
-                    .getComment(comment.id)
-                    .downvote()
-                    .catch(
-                        e =>
-                            moment
-                                .utc()
-                                .diff(
-                                    moment.unix(comment.created_utc),
-                                    "days"
-                                ) > 30
-                                ? console.log(
-                                      "This comment is 30 days old or older"
-                                  )
-                                : console.log(e)
-                    );
+            comments.map(function(comment) {
+                if (
+                    moment
+                        .utc()
+                        .diff(moment.unix(comment.created_utc), "days") > 30
+                ) {
+                    ++messages[index].olderThanThirty;
+                } else {
+                    return Promise.all([
+                        r
+                            .getComment(comment.id)
+                            .downvote()
+                            .then(++messages[index].successfullyDownvoted)
+                            .catch(function(err) {
+                                ++messages[index].fiveOhThreeErrors;
+                            })
+                    ]).catch(function(err) {
+                        console.log(
+                            `After downvoting, an error occurred: ${err}`
+                        );
+                    });
+                }
             })
         )
-        .then(() => console.log(messages))
-        .catch(e => console.log(e));
+        .then(function() {
+            count += array.length;
+            if (count === array.length * array.length) {
+                console.log(messages);
+                done();
+            }
+        })
+        .catch(function(err) {
+            if (err.statusCode === 503) {
+                console.log(
+                    `There was a 503 error (reddit is busy) when trying to downvote ${target}. Try getting petty with it later and make sure to spam the shit out of reddit admins in a petty way about how bad their servers are.`
+                );
+            } else {
+                console.log(err);
+            }
+            console.log(`This is what was done before the error ${JSON.stringify(messages, null, 4)}`);
+        });
 });
